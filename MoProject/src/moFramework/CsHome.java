d0b83e973e5d6863bb5b61b89d1af043ca08aba2
Mohammed.Shahriar <Mohammed.Shahriar@ASC-6HD0QF2.ad.ascendlearning.com>
2018-08-08 15:52:57 -0700

    csHome commit

---
+++ MoProject/src/moFramework/CsHome.java
@@ -0,0 +1,26 @@
+package moFramework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CsHome {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		String exePath = "C:\\Users\\mohammed.shahriar\\Desktop\\Tools\\chromedriver\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver chromeBrowser = new ChromeDriver();
+		chromeBrowser.get("http://www.clicksafety.com");
+		Thread.sleep(5000);
+		WebElement loginButton = chromeBrowser.findElement(By.id("btnLoginLink"));
+		if (loginButton.isDisplayed()) {
+			loginButton.click();
+			System.out.println("This CS homepage is up and running");
+		}
+		Thread.sleep(5000);
+		chromeBrowser.quit();
+	}
+
+}
