064c5e553578d1dc660e2b9af1536821e6e0abba
Mohammed.Shahriar <Mohammed.Shahriar@ASC-6HD0QF2.ad.ascendlearning.com>
2018-08-08 22:30:22 -0700

    added TestNG test

--- MoProject/src/moFramework/CsHome.java
+++ MoProject/src/moFramework/CsHome.java
@@ -1,25 +1,38 @@
 package moFramework;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class CsHome {
-
-	public static void main(String[] args) throws InterruptedException {
+	
+	@Test
+	public static void CsWww() {
 
 		String exePath = "C:\\Users\\mohammed.shahriar\\Desktop\\Tools\\chromedriver\\chromedriver.exe";
 		System.setProperty("webdriver.chrome.driver", exePath);
 		WebDriver chromeBrowser = new ChromeDriver();
 		chromeBrowser.get("http://www.clicksafety.com");
-		Thread.sleep(5000);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		WebElement loginButton = chromeBrowser.findElement(By.id("btnLoginLink"));
 		if (loginButton.isDisplayed()) {
 			loginButton.click();
 			System.out.println("This CS homepage is up and running");
 		}
-		Thread.sleep(5000);
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		chromeBrowser.quit();
 	}
 
