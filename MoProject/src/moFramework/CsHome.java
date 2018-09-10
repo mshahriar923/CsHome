ff295290757371d37872babc44ed95b71ae70b14
Mohammed.Shahriar <Mohammed.Shahriar@ASC-6HD0QF2.ad.ascendlearning.com>
2018-09-10 15:05:24 -0700

    Add a new Testcase for Login is working

--- MoProject/src/moFramework/CsHome.java
+++ MoProject/src/moFramework/CsHome.java
@@ -35,5 +35,44 @@ public class CsHome {
 		}
 		chromeBrowser.quit();
 	}
+	
+	@Test
+	public static void CsLogin() {
+
+		String exePath = "C:\\Users\\mohammed.shahriar\\Desktop\\Tools\\chromedriver\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", exePath);
+		WebDriver chromeBrowser = new ChromeDriver();
+		chromeBrowser.get("http://www.clicksafety.com");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		WebElement loginButton = chromeBrowser.findElement(By.id("btnLoginLink"));
+		if (loginButton.isDisplayed()) {
+			loginButton.click();
+			System.out.println("This CS homepage is up and running");
+		}
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		chromeBrowser.findElement(By.id("username")).sendKeys("qaitest1");
+		chromeBrowser.findElement(By.id("password")).sendKeys("1234");
+		chromeBrowser.findElement(By.id("login-submit")).click();
+		System.out.println("CS login is working");
+		chromeBrowser.quit();
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 
 }
